af5b3bd283b545c0051b04df1dc2f5df8ef649a6
ShahriarPriyo <shahriarpriyo98@gmail.com>
2022-04-17 21:01:51 +0600

    about us updated

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,7 +5,11 @@ const About = () => {
     return (
         <div>
             <Header></Header>
-            <h1>This is About</h1>
+            <div className='conatiner mx-auto py-5 px-5'>
+                <h1 className='text-danger text-center'>About Us</h1>
+                <p>As we are slowly walking towards the end of the course, we need to ensure proper practice of every module. After a long and hard working 6 months long course , I can realize my changes.But this is not the end. I need to continue my hard work and reach a specific goal to achieve success. In next 3 month, I want to see myself as a well prepared web developer who can cope up with any situation.</p>
+
+            </div>
         </div>
     );
 };
