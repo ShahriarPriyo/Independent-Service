7be1650a1679f1b7994b2a7247dc5768347e7f3a
ShahriarPriyo <shahriarpriyo98@gmail.com>
2022-04-18 21:18:04 +0600

    signup and login

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import React, { Component } from 'react';
-import Home from '../src/Pages/Home/Home';
-import About from '../src/Pages/About/About';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 import Blogs from './Pages/Blogs/Blogs';
+import NotFound from './Shared/NotFound/NotFound';
+import Login from './Pages/LoginSignup/Login/Login';
+import Signup from './Pages/LoginSignup/Signup/Signup';
+
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
         <Route path="/home" element={<Home></Home>} />
         <Route path="/about" element={<About />} />
         <Route path="/blogs" element={<Blogs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
 
     </div>
